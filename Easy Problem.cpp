699518e656f05c1867d8b993ab48e32fd0187c5c
SHREEHARI KALUNDIA <101623053+shreehari709@users.noreply.github.com>
2025-03-16 22:41:02 +0530

    Add files via upload

--- Easy Problem.cpp
+++ Easy Problem.cpp
@@ -3,15 +3,15 @@ using namespace std;
 int main()
 {
     int n;
-    double sum = 0;
     cin >> n;
+    int arr[n];
     for (int i = 0; i < n; i++)
     {
-        int p;
-        cin >> p;
-        sum += p;
+        cin >> arr[i];
     }
-    double result = sum / n;
-    cout << setprecision(12) << result;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] - 1 << endl;
+    }
     return 0;
 }
